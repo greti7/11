0a44af162f5b916f7e6c44587ee78e7acbb9b8ad
greti7 <149896563+greti7@users.noreply.github.com>
2024-12-05 10:47:51 +0200

    Add files via upload

---
+++ 2023 poterettsegi III.1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void DNPI(int n)
+{
+    cout << 1 << " ";
+    for(int i=3; i<n/2+1; i+=2)
+    {    bool prim=true;
+        if(n%i==0)
+        {
+            cout << i << " ";
+
+         for(int j=2; j<n/2+1; j++)
+         {
+            if(i%j!=0)
+            {
+               prim=false;
+            }
+            else{}
+         }
+         if(!prim)
+         {
+             cout << i << " ";
+         }
+    }
+}
+
+int main()
+{
+    DNPI(90);
+
+    return 0;
+}
