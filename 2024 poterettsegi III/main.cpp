04e90237d582341e61afc9c7733362a03632fdb3
greti7 <149896563+greti7@users.noreply.github.com>
2025-03-21 07:19:45 +0200

    Add files via upload

--- 2024 poterettsegi III/main.cpp
+++ 2024 poterettsegi III/main.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
 int main()
 {
-   int x,y,z,F,n;
-   cout << "x=";
-   cin >> x;
-   cout << "y=";
-   cin >> y;
-   cout << "z=";
-   cin >> z;
-   ofstream out("bac.txt");
-   for(int i=0;i<n;)
-
-
-
+  int n,m,N[100][100],i,j;
+  cout << "m=";
+  cin >> m;
+  cout << "n=";
+  cin >> n;
+  for(i=0;i<m;i++)
+  {
+      for(j=0;j<n;j++)
+      {
+           cout << "N["<<i<<"]["<<j<<"]=";
+           cin>> N[i][j];
+      }
+  }
+ for(int i=1;i<m;i+=2)
+ {
+     int min = N[i][0];
+     for(int j=1;j<n;j++)
+     {
+         if(N[i][j]<min)
+         {
+             min=N[i][j];
+         }
+     }
+     for(int j=0;j<n;j++)
+     {
+         if(N[i-1][j]>min-1)
+         {
+             N[i-1][j]=min-1;
+         }
+     }
+ }
+ for(int i=0;i<m;i++)
+ {
+     for(int j=0;j<n;j++)
+     {
+         cout << N[i][j]<< " ";
+     }
+ }
     return 0;
 }
