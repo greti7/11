a5c126b3ffa7acb054cc95966735d594b2c272b2
greti7 <149896563+greti7@users.noreply.github.com>
2025-03-21 07:17:50 +0200

    Add files via upload

--- quicksort0/main.cpp
+++ quicksort0/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 
 using namespace std;
+void quicksort(int arr[],int balindex, int jobbindex)
+{
+    int i=balindex;
+    int j=jobbindex;
+    int pivot=arr[(balindex+jobbindex)/2;];
+    while(i<=j)
+    {
+        while(arr[i]<pivot)
+        {
+            i++;
+        }
+         while(arr[i]>pivot)
+        {
+            j--;
+        }
+        if(i<=j)
+        {
+            int tmp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp;
+            i++;
+            j--;
+        }
+
+    }
+    if(balindex<j)
+    {
+        quicksort(arr,balindex,j);
+    }
+    if(jobbindex>i)
+    {
+        quicksort(arr,i,jobbindex);
+    }
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
