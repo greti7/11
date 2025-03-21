da0cae22e20f3a8e7645b57ad0b04861f93bbd4f
greti7 <149896563+greti7@users.noreply.github.com>
2025-03-21 07:19:27 +0200

    Add files via upload

---
+++ 2024 poterettsegi III/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+   int x,y,z,F,n;
+   cout << "x=";
+   cin >> x;
+   cout << "y=";
+   cin >> y;
+   cout << "z=";
+   cin >> z;
+   ofstream out("bac.txt");
+   for(int i=0;i<n;)
+
+
+
+    return 0;
+}
